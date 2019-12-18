79bb8bde5c42000cb372c98b768e35961ac6bbdb
David Cai <57420559+david-cai@users.noreply.github.com>
2019-12-18 14:26:10 -0500

    Updated site with shop page as well as product preview component and item component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './App.css';
 import HomePage from '../src/pages/homepage/homepage.component';
+import ShopPage from '../src/pages/shop/shop.component';
+import {Switch, Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div> 
-      <HomePage />
+      <Switch>
+        <Route exact path ='/' component={HomePage}/>
+        <Route path ='/shop' component={ShopPage}/>
+      </Switch>
+
     </div>
   );
 }
